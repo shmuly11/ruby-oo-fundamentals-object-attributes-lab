e2debf9e88871931ba6e658678792923c08ef122
shmuly11 <shmuly11@gmail.com>
2020-12-08 14:00:17 -0500

    commit tree 8026e7a89d3f3ae6b6e940e98ab4a675d4005cd8

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Dog
+    def initialize
+        @name = name
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(new_breed)
+        @breed = new_breed
+    end
+end
+
+#ruby = Dog.new("Scooby", "Chiwowa")
+#ruby.name = "Jack"
+#ruby.breed = "Kito"
+#binding.pry
+#0
